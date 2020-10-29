2872ff0a74f82c2a5a7a6aef72ab27eabbeda4c9
Nikhil Gupta <nikhil062000@gmail.com>
2020-10-29 21:23:02 +0530

    Updated scanner part2

--- PythonForPenetrationTesting/scanner.py
+++ PythonForPenetrationTesting/scanner.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import nmap
 
 scanner = nmap.PortScanner()
@@ -15,5 +14,29 @@ type(ip_addr)
 resp = input("""\nPlease enter the type of scan you want to run
                 1)SYN ACK Scan
                 2)UDP Scan
-                3)Comprehensive Scan""")
+                3)Comprehensive Scan\n""")
 print("You have selected option: ", resp)
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024','-v -sS')
+    print(scanner.scaninfo())
+    print("IP status: ",scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ",scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024','-v -sU')
+    print(scanner.scaninfo())
+    print("IP status: ",scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ",scanner[ip_addr]['udp'].keys())
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024','-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("IP status: ",scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ",scanner[ip_addr]['tcp'].keys())
+else:
+    print("Please enter a Valid option")
